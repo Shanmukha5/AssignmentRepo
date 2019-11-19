efec03059b5f18e29190e8ed895ca6c771bd5ca1
Shanmukha5 <shanmukhaprakash333@gmail.com>
2019-11-19 15:32:21 +0530

    Everything is done

--- menufile.py
+++ menufile.py
@@ -1,2 +1,19 @@
 #This is menuFile
-print("<-- Program for Addition,Subtraction,Multiplication,Division and modulus -->")
+import additionFile, subtractionFile, multiplicationFile, divisionFile, modulusFile
+
+
+
+print("<-- Mathematical Program -->\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus")
+inp = int(input("Enter your choice->"))
+if(inp==1):
+	print(additionFile.add())
+elif(inp==2):
+	print(subtractionFile.subt())
+elif(inp==3):
+	print(multiplicationFile.multi())
+elif(inp==4):
+	print(divisionFile.div())
+elif(inp==5):
+	print(modulusFile.mod())
+else:
+	print("Invalid choice")
